660f885e6772c62c35c82e3fd5971e9a46d0f57a
macmata <info@alexandre-leblanc.com>
2014-06-03 23:41:31 -0400

    * added touch pad * added  assets for touchpad

--- core/src/com/projet5001/game/Menu.java
+++ core/src/com/projet5001/game/Menu.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
 
 public class Menu implements Screen {
@@ -25,6 +25,13 @@ public class Menu implements Screen {
     Label label;
     Director director;
     TextButton button;
+    Table table;
+    private Touchpad touchpad;
+    private Touchpad.TouchpadStyle touchpadStyle;
+    private Skin touchpadSkin;
+    private Drawable touchBackground;
+    private Drawable touchKnob;
+    private Texture blockTexture;
 
     public Menu(final Projet5001 game) {
 
@@ -45,11 +52,15 @@ public class Menu implements Screen {
         // un skin contient toutes les information pour cree le ui d'un boutton, voir le dossier json
         skin = new Skin(Gdx.files.internal("data/button/uiskin.json"));
 
+
         //Le textButton qui contient un text et un skin avec le param default (voir fichier json)
         button = new TextButton("Lance le jeux", skin, "default");
         button.setPosition(100, 100);
-
-
+        table = new Table();
+        table.debug();
+        table.setFillParent(true);
+        table.add(button);
+        table.add(label);
         button.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 //Lance le scree test
@@ -59,9 +70,27 @@ public class Menu implements Screen {
         });
 
 
+        touchpadSkin = new Skin();
+        //Set background image
+        touchpadSkin.add("touchBackground", new Texture("data/touchpad/touchBackground.png"));
+        //Set knob image
+        touchpadSkin.add("touchKnob", new Texture("data/touchpad/touchKnob.png"));
+        //Create TouchPad Style
+        touchpadStyle = new Touchpad.TouchpadStyle();
+        //Create Drawable's from TouchPad skin
+        touchBackground = touchpadSkin.getDrawable("touchBackground");
+        touchKnob = touchpadSkin.getDrawable("touchKnob");
+        //Apply the Drawables to the TouchPad Style
+        touchpadStyle.background = touchBackground;
+        touchpadStyle.knob = touchKnob;
+        //Create new TouchPad with the created style
+        touchpad = new Touchpad(10, touchpadStyle);
+        //setBounds(x,y,width,height)
+        touchpad.setBounds(50, 50, 200, 200);
+
         director = new Director();
-        director.addActor(label);
-        director.addActor(button);
+        director.addActor(table);
+        director.addActor(touchpad);
 
     }
 
@@ -77,8 +106,13 @@ public class Menu implements Screen {
         shapeRenderer.rect(0, 0, 50, 50);
         shapeRenderer.end();
 
+
+        table.setX(table.getX() + touchpad.getKnobPercentX());
+        table.setY(table.getY() + touchpad.getKnobPercentY());
+
         game.batcher.begin();
         director.draw();
+        Table.drawDebug(director);
         game.batcher.end();
 
 
--- core/src/com/projet5001/game/Projet5001.java
+++ core/src/com/projet5001/game/Projet5001.java
@@ -1,6 +1,8 @@
 package com.projet5001.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
@@ -10,9 +12,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class Projet5001 extends Game {
     public SpriteBatch batcher;
+    Application.ApplicationType osType;
     @Override
     public void create () {
         batcher = new SpriteBatch();
+        osType = Gdx.app.getType();
+
         /**
          * pour le moment je ne lance que cette fenetre de test mais ca donne un bon exemple.
          * voir https://github.com/libgdx/libgdx-demo-superjumper pour d'autre exemple
