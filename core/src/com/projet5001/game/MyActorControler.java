915ebce54012eeaded3cb3e99694382782e8b4d7
maximegir <maxime.g.girard@gmail.com>
2014-06-22 18:05:39 -0400

    essaie

--- core/src/com/projet5001/game/MyActorControler.java
+++ core/src/com/projet5001/game/MyActorControler.java
@@ -7,10 +7,11 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
  * Created by macmata on 31/05/14.
  */
 public class MyActorControler {
-    public static void register(MyActor myActor) {
+    public static void register(final MyActor myActor) {
         myActor.addListener(new InputListener() {
             public boolean keyDown(InputEvent event, int keycode) {
-                System.out.println("test");
+                System.out.println("test" + " : " + keycode);
+                myActor.move(5,0);
                 return false;
             }
         });
