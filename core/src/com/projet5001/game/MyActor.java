0a033cf5ceea13526a34baad8e225e6be8a16d40
macmata <info@alexandre-leblanc.com>
2014-06-27 09:20:26 -0400

    premier ajout du systeme de collision

--- core/src/com/projet5001/game/MyActor.java
+++ core/src/com/projet5001/game/MyActor.java
@@ -2,9 +2,8 @@ package com.projet5001.game;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 
 
@@ -13,7 +12,7 @@ public class MyActor extends Actor {
     private int slowSpeed;
     private int speed;
     private Sprite sprite;
-
+    private Rectangle hitbox;
     public MyActor(Sprite sprite) {
         super();
         this.speed = 1;
@@ -23,40 +22,51 @@ public class MyActor extends Actor {
         this.setBounds(this.sprite.getX(),this.sprite.getY(),this.sprite.getWidth(),this.sprite.getHeight());
         this.setOrigin(this.sprite.getWidth() / 2, this.sprite.getHeight() / 2);
         this.setTouchable(Touchable.enabled);
-        addListener(new MovementListener(){
+        this.hitbox = new Rectangle(this.getX(), this.getY(),this.getWidth(), this.getHeight());
+        addListener(new MovementListener() {
 
             public boolean moveLeft(MovementEvents event) {
-                ((MyActor)event.getTarget()).moveLeft();
+                ((MyActor) event.getTarget()).moveLeft();
                 return false;
             }
+
             public boolean moveRight(MovementEvents event) {
-                ((MyActor)event.getTarget()).moveRight();
+                ((MyActor) event.getTarget()).moveRight();
                 return false;
             }
+
             public boolean moveUp(MovementEvents event) {
-                ((MyActor)event.getTarget()).moveUp();
+                ((MyActor) event.getTarget()).moveUp();
                 return false;
             }
+
             public boolean moveDown(MovementEvents event) {
-                ((MyActor)event.getTarget()).moveDown();
+                ((MyActor) event.getTarget()).moveDown();
                 return false;
             }
+
             public boolean idle(MovementEvents event) {
-                ((MyActor)event.getTarget()).setSpeed(0);
+                ((MyActor) event.getTarget()).setSpeed(0);
                 return false;
             }
+
             public boolean moveFast(MovementEvents event) {
-                ((MyActor)event.getTarget()).setSpeed(fastSpeed);
+                ((MyActor) event.getTarget()).setSpeed(fastSpeed);
                 return false;
             }
+
             public boolean moveSlow(MovementEvents event) {
-                ((MyActor)event.getTarget()).setSpeed(slowSpeed);
+                ((MyActor) event.getTarget()).setSpeed(slowSpeed);
                 return false;
             }
 
         });
     }
-
+    public void act(float delta){
+        super.act(delta);
+        this.hitbox.setSize(this.getWidth(),this.getHeight());
+        this.hitbox.setPosition(this.getX(), this.getY());
+    }
     public int getSpeed(){
         return this.speed;
     }
@@ -84,23 +94,14 @@ public class MyActor extends Actor {
 
     public void moveLeft(){
         move(-speed,0);
-        System.out.println(getX());
-        System.out.println(this.sprite.getX());
     }
     public void moveRight(){
         move(speed,0);
-        System.out.println(getX());
-        System.out.println(this.sprite.getX());
     }
     public void moveUp(){
         move(0, speed);
-        System.out.println(getY());
-        System.out.println(this.sprite.getY());
-
     }
     public void moveDown(){
         move(0,-speed);
-        System.out.println(getY());
-        System.out.println(this.sprite.getY());
     }
 }
