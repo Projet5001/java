983a609bea2ec5e7e3208d16a1dfa3ab2df2f441
macmata <info@alexandre-leblanc.com>
2014-06-30 16:59:02 -0400

    debut de la creation du collision manager

---
+++ core/src/com/projet5001/game/Collisions/WordCollisionControleur.java
@@ -0,0 +1,13 @@
+package com.projet5001.game.Collisions;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+/**
+ * Created by macmata on 29/06/14.
+ */
+public class WordCollisionControleur {
+    private Group group;
+    public WordCollisionControleur(Group actors) {
+        this.group = actors;
+    }
+}
