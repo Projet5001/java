5f3b3448ebe19aa7733206163343e1bad76ab75d
macmata <info@alexandre-leblanc.com>
2014-07-01 10:51:00 -0400

    mod pour collision

---
+++ core/src/Collisions/WorldColletor.java
@@ -0,0 +1,16 @@
+package Collisions;
+
+import com.badlogic.gdx.scenes.scene2d.Group;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
+public class WorldColletor {
+    private Group group;
+    private WorldColletor child;
+    private HashMap<Integer,LinkedList> collection;
+    public WorldColletor() {
+
+    }
+}
