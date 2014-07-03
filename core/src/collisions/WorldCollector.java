6d17f53558b29535cf50dbb1237c497cf23b8038
macmata <info@alexandre-leblanc.com>
2014-07-03 00:21:45 -0400

    ebauche du systeme de collison . ce systeme verifie la collision specifiquement etre un acteur et tous les autrs

--- core/src/collisions/WorldCollector.java
+++ core/src/collisions/WorldCollector.java
@@ -1,40 +1,77 @@
 package collisions;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.utils.IntMap;
+import actors.MyActor;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 
 public class WorldCollector {
-    private Group group;
-    private WorldCollector child;
-    private IntMap<LinkedList<Actor>> collection;
+
+    private HashMap<Vector2,LinkedList<MyActor>> collection ;
     public WorldCollector() {
-        this.collection = new IntMap<LinkedList<Actor>>(10);
+        this.collection = new HashMap<Vector2, LinkedList<MyActor>>();
     }
 
-    public void add(Actor actor){
-        LinkedList<Actor> keyList;
-        double x = Math.floor(actor.getX()/32);
-        double y = Math.floor(actor.getY()/32);
-        double l = 0;
-        double w = 0;
-        for (int i = 0 ; i< Math.ceil(actor.getWidth()/32); i++){
+    public void add(MyActor actor){
+        LinkedList<MyActor> keyList;
+        float x = (float)Math.floor(actor.getX()/64);
+        float y = (float)Math.floor(actor.getY()/64);
+        float l = 0;
+        float w = 0;
+        Vector2 v = new Vector2();
+        for (int i = 0 ; i< Math.ceil(actor.getWidth()/64); i++){
             x += l;
-            keyList = this.collection.get((int)x);
-            if (keyList != null){
-                keyList.add(actor);
-                this.collection.remove((int)x);
-                this.collection.put((int)x,keyList);
-            }else{
-
-                keyList = new LinkedList<Actor>();
-                keyList.add(actor);
-                this.collection.put((int)x,keyList);
+            for (int j = 0 ; j< Math.ceil(actor.getHeight()/64); j++) {
+                y += w;
+                v.set(x,y);
+                keyList = this.collection.get(v);
+                if (keyList != null) {
+                    if (!keyList.contains(actor)) {
+                        keyList.add(actor);
+                        this.collection.remove(v);
+                        this.collection.put(v, keyList);
+                    }
+                } else {
+                    keyList = new LinkedList<MyActor>();
+                    keyList.add(actor);
+                    this.collection.put(v, keyList);
+                }
+                w+=64;
             }
-
-            l+=32;
+            l+=64;
         }
+    }
+
+    public boolean isHit (Vector2 v){
+        return this.collection.containsKey(v);
+    }
+
+
+    private Vector2 getHashFromVector(MyActor actor){
+        float x = (float)Math.floor(actor.getX()/64);
+        float y = (float)Math.floor(actor.getY()/64);
+        return new Vector2(x,y);
+    }
+
+    /**
+     * version 1
+     * @param actor
+     * @return the actor that hit the actor
+     */
+    public MyActor hit(MyActor actor){
+        Vector2 vector2 = getHashFromVector(actor);
+        if (isHit(vector2)){
+            LinkedList<MyActor> keyList = this.collection.get(vector2);
+            Rectangle rect =  new Rectangle();
+            for (MyActor aKeyList : keyList) {
+                if (Intersector.intersectRectangles(actor.getHitbox(), aKeyList.getHitbox(), rect)) {
+                    return aKeyList;
+                }
+            }
+        }
+        return null;
     }
 }
