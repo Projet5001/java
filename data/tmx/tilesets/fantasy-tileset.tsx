daae7cee7df75bdc01dbfad5a9a47e9f3ed80269
macmata <info@alexandre-leblanc.com>
2014-07-03 11:33:54 -0400

    remove doublons

--- data/tmx/tilesets/fantasy-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="fantasy-tileset" tilewidth="32" tileheight="32">
- <image source="fantasy-tileset.png" trans="ff00ff" width="256" height="832"/>
-</tileset>
